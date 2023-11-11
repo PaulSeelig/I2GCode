71f5521c76a17edbc110150b5bcebfe69e5c628d
Pierre <pierre.wiele@student.htw-berlin.de>
2023-11-11 12:59:01 +0100

    History

---
+++ GUI I2G/History.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI_I2G
+{
+    interface IHistoryEntry
+    {
+        public void SaveGcodeProject()
+        {
+
+        }
+
+    }
+
+    internal class History
+    {
+        public string ProjectName { get; private set; } = DateTime.Now.ToString();
+        public static string GCodeSavePath { get; private set; } = @".\HistoryEntries\";
+
+        public Parameter Parameter { get; private set; }
+
+        public GCode Gcodes { get; private set; }
+
+        public Image? Image { get; private set; }
+    }
+}
