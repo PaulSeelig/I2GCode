86423de4280c1bfc8f43a44bb65c5d94e119fb40
Pierre <pierre.wiele@student.htw-berlin.de>
2023-11-11 12:23:35 +0100

    Conture-Classes

---
+++ GUI I2G/Conture.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI_I2G
+{
+    internal abstract class Conture
+    {
+        public Point StartPoint { get; set; }
+        public Point EndPoint { get; set; }
+
+        public static Conture[] ContourExtractor()
+        {
+            //bloss beispielhaft
+            Conture[] result = new Conture[3];
+            result[0] = new Vector(new Point(3, 5) , new Point(2, -4));
+            result[1] = new Curve(new Point(3, 5), new Point(3, 6), 5);
+            result[2] = new Vector(new Point(4, 5), new Point(7, 4));
+
+            return result;
+        }
+        public static void WriteOutContures(Conture[] contures)
+        {
+            foreach (Conture conture in contures) 
+            {
+                Console.WriteLine(conture.StartPoint.ToString() + " : " + conture.EndPoint.ToString());
+                if (conture.GetType() == typeof(Curve))
+                    Console.Write(" : " + (conture as Curve).Radius.ToString());
+                if (conture is Vector)
+                    Console.WriteLine((conture as Vector).Length.ToString());
+            }
+        }
+    }
+
+    class Vector : Conture
+    {
+        public double Length { get; set; }
+
+        public Vector(Point start, Point end) 
+        {
+            StartPoint = start;  
+            EndPoint = end;
+        }
+    }
+
+    class Curve : Conture 
+    {
+        public double Radius { get; set; }
+
+        public Curve(Point start, Point end, double radius)
+        {
+            StartPoint = start;
+            EndPoint = end;
+            Radius = radius;
+        }
+    }
+}
