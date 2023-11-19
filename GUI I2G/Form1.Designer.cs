982961c7ef066b67977837c955f8e856876a9f64
s0578706 <s0578706@htw-berlin.de>
2023-11-19 14:30:03 +0100

    Textbox und label geändert vorheriges war nur projekt name

--- GUI I2G/Form1.Designer.cs
+++ GUI I2G/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace GUI_I2G
 {
-    partial class Form1
+    partial class I2Gcode
     {
         /// <summary>
         ///  Required designer variable.
@@ -30,87 +30,93 @@
         {
             genButton = new Button();
             displayBox = new PictureBox();
-            label1 = new Label();
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
-            textBox3 = new TextBox();
-            label2 = new Label();
+            Instructions = new Label();
+            Xcordfield = new TextBox();
+            TBDycord = new TextBox();
+            TBDzcord = new TextBox();
+            Xlabel = new Label();
             ((System.ComponentModel.ISupportInitialize)displayBox).BeginInit();
             SuspendLayout();
             // 
-            // button1
+            // genButton
             // 
             genButton.BackColor = SystemColors.MenuHighlight;
-            genButton.Location = new Point(87, 335);
-            genButton.Name = "button1";
-            genButton.Size = new Size(193, 60);
+            genButton.Location = new Point(76, 251);
+            genButton.Margin = new Padding(3, 2, 3, 2);
+            genButton.Name = "genButton";
+            genButton.Size = new Size(169, 45);
             genButton.TabIndex = 0;
             genButton.Text = "G- Code generieren";
             genButton.UseVisualStyleBackColor = false;
             genButton.Click += button1_Click;
             // 
-            // pictureBox1
+            // displayBox
             // 
             displayBox.BackColor = SystemColors.GradientInactiveCaption;
-            displayBox.Location = new Point(12, 12);
-            displayBox.Name = "pictureBox1";
-            displayBox.Size = new Size(776, 154);
+            displayBox.Location = new Point(10, 9);
+            displayBox.Margin = new Padding(3, 2, 3, 2);
+            displayBox.Name = "displayBox";
+            displayBox.Size = new Size(679, 116);
             displayBox.TabIndex = 1;
             displayBox.TabStop = false;
             // 
-            // label1
+            // Instructions
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(275, 130);
-            label1.Name = "label1";
-            label1.Size = new Size(247, 20);
-            label1.TabIndex = 2;
-            label1.Text = "Bild hier via Drag n Drop hochladen";
+            Instructions.AutoSize = true;
+            Instructions.Location = new Point(241, 98);
+            Instructions.Name = "Instructions";
+            Instructions.Size = new Size(194, 15);
+            Instructions.TabIndex = 2;
+            Instructions.Text = "Bild hier via Drag n Drop hochladen";
             // 
-            // textBox1
+            // Xcordfield
             // 
-            textBox1.Location = new Point(155, 216);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(125, 27);
-            textBox1.TabIndex = 3;
+            Xcordfield.Location = new Point(136, 162);
+            Xcordfield.Margin = new Padding(3, 2, 3, 2);
+            Xcordfield.Name = "Xcordfield";
+            Xcordfield.Size = new Size(110, 23);
+            Xcordfield.TabIndex = 3;
             // 
-            // textBox2
+            // TBDycord
             // 
-            textBox2.Location = new Point(155, 249);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(125, 27);
-            textBox2.TabIndex = 4;
+            TBDycord.Location = new Point(136, 187);
+            TBDycord.Margin = new Padding(3, 2, 3, 2);
+            TBDycord.Name = "TBDycord";
+            TBDycord.Size = new Size(110, 23);
+            TBDycord.TabIndex = 4;
             // 
-            // textBox3
+            // TBDzcord
             // 
-            textBox3.Location = new Point(155, 282);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(125, 27);
-            textBox3.TabIndex = 5;
+            TBDzcord.Location = new Point(136, 212);
+            TBDzcord.Margin = new Padding(3, 2, 3, 2);
+            TBDzcord.Name = "TBDzcord";
+            TBDzcord.Size = new Size(110, 23);
+            TBDzcord.TabIndex = 5;
             // 
-            // label2
+            // Xlabel
             // 
-            label2.AutoSize = true;
-            label2.Location = new Point(42, 216);
-            label2.Name = "label2";
-            label2.Size = new Size(88, 20);
-            label2.TabIndex = 6;
-            label2.Text = "X- Startwert";
+            Xlabel.AutoSize = true;
+            Xlabel.Location = new Point(37, 162);
+            Xlabel.Name = "Xlabel";
+            Xlabel.Size = new Size(69, 15);
+            Xlabel.TabIndex = 6;
+            Xlabel.Text = "X- Startwert";
             // 
-            // Form1
+            // I2Gcode
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(label2);
-            Controls.Add(textBox3);
-            Controls.Add(textBox2);
-            Controls.Add(textBox1);
-            Controls.Add(label1);
+            ClientSize = new Size(700, 338);
+            Controls.Add(Xlabel);
+            Controls.Add(TBDzcord);
+            Controls.Add(TBDycord);
+            Controls.Add(Xcordfield);
+            Controls.Add(Instructions);
             Controls.Add(displayBox);
             Controls.Add(genButton);
-            Name = "Form1";
-            Text = "Form1";
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "I2Gcode";
+            Text = "I2G-code";
             ((System.ComponentModel.ISupportInitialize)displayBox).EndInit();
             ResumeLayout(false);
             PerformLayout();
@@ -120,10 +126,10 @@
 
         private Button genButton;
         private PictureBox displayBox;
-        private Label label1;
-        private TextBox textBox1;
-        private TextBox textBox2;
-        private TextBox textBox3;
-        private Label label2;
+        private Label Instructions;
+        private TextBox Xcordfield;
+        private TextBox TBDycord;
+        private TextBox TBDzcord;
+        private Label Xlabel;
     }
 }
