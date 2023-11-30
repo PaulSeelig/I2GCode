e34934e4e95ef001a60053b2fec6c75f52f6e30f
Pierre <pierre.wiele@student.htw-berlin.de>
2023-11-30 12:45:48 +0100

    GUnit-Test

---
+++ GCode_Test/UnitTest.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using GUI_I2G.GCodeclasses;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GCode_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            GCode_Test 
+        }
+    }
+}
