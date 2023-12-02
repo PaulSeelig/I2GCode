93bf4c7fd6d15ef8b1ca0bd7705f8a4816887837
s0578706 <s0578706@htw-berlin.de>
2023-12-02 14:48:31 +0100

    History class added to help with saving and iterating through saved projects

---
+++ GUI I2G/History.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class History : IHistorySafe
+{
+	//Properties:
+	//private s´o only inhouse methods can access it
+	private List<HistoryEntry> history = new List<HistoryEntry> ();
+
+	//Methods:
+
+	public HistoryEntry GetEntries()
+	{ 
+		return history; 
+	}
+
+	public override void SafeGcodeProject()
+	{
+
+	}
+
+	public History()
+	{
+	}
+}
