4634ee5a7ea31292f1c01234f5500433a2992c10
s0578706 <s0578706@htw-berlin.de>
2023-12-07 00:11:32 +0100

    History Unit test project

---
+++ HistoryTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+using 
+
+namespace HistoryTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
