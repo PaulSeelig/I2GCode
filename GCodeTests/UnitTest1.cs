a2b4608bae9a2636f4bf8af07f333d143704d90f
Pierre <pierre.wiele@student.htw-berlin.de>
2023-12-07 13:19:52 +0100

    Unittests_Updated

---
+++ GCodeTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace GCodeTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
