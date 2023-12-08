bea25b1a6fc5240d65ace61eddf3ac7ce725bd76
s0578706 <s0578706@htw-berlin.de>
2023-12-08 14:59:30 +0100

    HistoryEntry Unittest Done

--- History Test/UnitTest1.cs
+++ History Test/UnitTest1.cs
@@ -2,7 +2,7 @@ using GUI_I2G;
 
 namespace History_Test
 {
-    public class THistoryEntryTest
+    public class HistoryEntryTest
     {
         [SetUp]
         public void Setup()
@@ -14,10 +14,21 @@ namespace History_Test
         public void ConstructorTest_All_All()
         {
             //Assemble
-            var entry = new HistoryEntry();
+            string projName = "TestName";
+            var param = new Parameter();
+            var GPath = new string(string.Empty);
+            var ImgPath = new string(string.Empty);
+
             //Act
+            var entry = new HistoryEntry(projName,param,GPath,ImgPath);
+            entry.UpdateLastOpened();
+
             //Assert
-            Assert.Pass();
+            Assert.AreEqual(projName, entry.projectName);
+            Assert.AreEqual(param, entry.parameter);
+            Assert.AreEqual(GPath, entry.gcodePath);
+            Assert.AreEqual(ImgPath, entry.imagePath);
+            Assert.AreEqual(DateTime.Today, entry.lastOpened);
         }
     }
 }
