ed046d5ee25375f73eccd8272388672aa29d6fdd
Leonardo <Leonardo.Mateljan@htw-berlin.de>
2024-01-04 14:09:53 +0100

    eine andere Methode + ein ansatz bei dem bogen erkannt werden nicht ganz fertig soll ich aber auch nicht machen :(

--- GUI I2G/Tests/Konturerkennung_Test_1.cs
+++ GUI I2G/Tests/Konturerkennung_Test_1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Emgu.CV.CvEnum;
+using static System.Windows.Forms.LinkLabel;
 
 namespace GUI_I2G.Tests
 {
@@ -15,86 +16,78 @@ namespace GUI_I2G.Tests
         //Eigenschaften
         public int Schwellenwert { get; set; }//threshold
                                               //Methoden
-        public VectorOfVectorOfPoint Konturen()
+        public void Konturen()
         {
+            Mat img = CvInvoke.Imread("Bild.png", Emgu.CV.CvEnum.ImreadModes.Color);
 
-            Mat img = CvInvoke.Imread("Bild.png", Emgu.CV.CvEnum.ImreadModes.Color);//um Objekt Matrix von Bild //man kann es auch hier schon Grayscalen mit ImreadModes.Grayscale                              
+            Image<Gray, System.Byte> imggray = new Image<Gray, System.Byte>(img);
 
-            Image<Gray, System.Byte> imggray = new Image<Gray, System.Byte>(img);//hier wird in Grayscale Format Image
-            //System.Byte für die Tiefe (jeder Pixel hat 8Bit um eine graufrabe(die farbe habe ich vorher gennant) zu sein)
-            Image<Gray, System.Byte> imageDraw = imggray.Copy();//auf dem werden die Konturen gemalt
+            Image<Gray, System.Byte> imageDraw = imggray.Copy();//auf dem werden die lKonturen gemalt            
 
-            //Linien dicker machen
-            Image<Gray, System.Byte> imageDilate = imggray.Copy();
-            //CvInvoke.Dilate(imggray,imageDilate,);
-
-            //Simple Thresholding wo man ein Threshold (Schwellenwert) angeben muss (eine Grenze)
-            //imggray = imggray.ThresholdBinary(new Gray(254), new Gray(255));
-            //imggray = imggray.ThresholdBinaryInv(new Gray(254), new Gray(255));//<=ist gedreht
-
-
-            //Adaptive Thresholding
-            //imggray = imggray.ThresholdAdaptive(new Gray(255), Emgu.CV.CvEnum.AdaptiveThresholdType.GaussianC,Emgu.CV.CvEnum.ThresholdType.Binary,11,new Gray(2));//11 ist die kasten größe ist auch (ca.11-13)standart (bei bestimmten Bilder sollte man das anpassen können(oder selbstständig))
-            //imggray = imggray.ThresholdAdaptive(new Gray(255), Emgu.CV.CvEnum.AdaptiveThresholdType.MeanC, Emgu.CV.CvEnum.ThresholdType.Binary,11,new Gray(2));//MeanC => echt schlecht
-
-
-            //Otsu Binarization
-            //imggray = imggray.ThresholdAdaptive(new Gray(255), Emgu.CV.CvEnum.AdaptiveThresholdType.MeanC, Emgu.CV.CvEnum.ThresholdType.Otsu, 11, new Gray(2));//Emgu.CV.Util.CvException: "OpenCV: Unknown/unsupported threshold type"
             double otsuSchwellenwert = CvInvoke.Threshold(imggray, imggray, 0, 255, ThresholdType.Otsu);
-            Console.WriteLine(otsuSchwellenwert);
-            //imggray = imggray.ThresholdBinary(new Gray(otsuSchwellenwert), new Gray(255));
 
+            VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+            CvInvoke.FindContours(imggray, contours, null, Emgu.CV.CvEnum.RetrType.List, Emgu.CV.CvEnum.ChainApproxMethod.ChainApproxNone);
 
-            //Zeig das binäre Bild
-            //CvInvoke.NamedWindow("Bild");
-            Image<Gray, System.Byte> imggray_resize = imggray.Copy();
-            CvInvoke.Resize(imggray, imggray_resize, new Size(1024, (int)(((double)img.Height / img.Width) * 1024)));
-            CvInvoke.Imshow("Bild_Binaer", imggray_resize);
-            CvInvoke.WaitKey(0);//warten bis eine Taste gedrückt wird sonnst geht es nicht er wird unendlich laden
-            CvInvoke.DestroyWindow("Bild_Binaer"); // macht das Fenster weg
-
-
-            //detect the contours            
-            VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();//wo die Lösung hin kommt
-            CvInvoke.FindContours(imggray, contours, null, Emgu.CV.CvEnum.RetrType.List, Emgu.CV.CvEnum.ChainApproxMethod.ChainApproxNone);//muss External noch verändern
-            /* RetrType (Contour retrieval mode):
-               External	0	Retrieve only the extreme outer contours
-	           List	1	Retrieve all the contours and puts them in the list
-	           Ccomp	2	Retrieve all the contours and organizes them into two-level hierarchy: top level are external boundaries of the components, second level are bounda boundaries of the holes
-	           Tree	3	Retrieve all the contours and reconstructs the full hierarchy of nested contours
-	           Floodfill	4	Flood fill type*/
-
-            //Malle die Konturen
             CvInvoke.DrawContours(imageDraw, contours, -1, new MCvScalar(200, 45, 45), 2);
-            //contourIdx: -1 bedeutet alle Konturen zeichenen
-            //MCvScalar: in welcher Farbe gemalt werden soll
-            //thickness: 2 This is the thickness of contour points
 
+            CvInvoke.Imwrite("DrawlName.png", imageDraw);
 
-            /*//Zeig die imageDraw
-            Image<Gray, System.Byte> imageDraw_resize = imageDraw.Copy();
-            CvInvoke.Resize(imageDraw, imageDraw_resize, new Size(1024, (int)(((double)img.Height / img.Width) * 1024)));
-            CvInvoke.Imshow("Bild_KonturenAufgemalt", imageDraw_resize);
-            CvInvoke.DestroyWindow("Bild_KonturenAufgemalt");
-            CvInvoke.WaitKey(0);
-            */
+            //Bogen erkennung
 
-
-            CvInvoke.Imwrite("DrawName.png", imageDraw);
-
-            Point[][] ablage = contours.ToArrayOfArray();
-            
-            
-            /*
-            for (int i = 0; i < ablage.GetLength(0); i++)
+            Point[][] konturen = contours.ToArrayOfArray();
+            List<Point> konturen_ = new List<Point>();
+            for (int i = 0; i < konturen.Length; i++)
             {
-                if (ablage[i][0] == ablage[i + 1][0])
+                for (int j = 0; j < konturen[i].Length; j++)
                 {
-
+                    konturen_.Add(konturen[i][j]);
                 }
             }
-            */
-            return contours;
+            Point[] konturen__ = konturen_.ToArray();
+
+            VectorOfPoint approxK = new VectorOfPoint();
+            VectorOfPoint preapproxKurve = new VectorOfPoint();
+            preapproxKurve.Push(konturen__);
+
+            CvInvoke.ApproxPolyDP(preapproxKurve, approxK, 0.5, false);
+            Point[] approxKurve = approxK.ToArray();
+
+            for (int i = 0; i < approxKurve.Length; i++)
+            {
+                if (i + 3 == approxKurve.Length)
+                    break;
+                Point mP = Mittelpunktrechner(approxKurve[i], approxKurve[i + 1], approxKurve[i + 2]);//Gleichung kontrolieren
+                //Abstand
+                double radius = Math.Sqrt(Math.Pow(mP.X - approxKurve[i].X, 2) + Math.Pow(mP.Y - approxKurve[i].Y, 2));
+                double p4Abstand = Math.Sqrt(Math.Pow(mP.X - approxKurve[i + 3].X, 2) + Math.Pow(mP.Y - approxKurve[i + 3].Y, 2));
+                //muss noch wegen Vorzeichen anpassen
+                if (radius == p4Abstand)
+                {
+                    //hier wurde ein Bogen erkannt
+                }
+            }
         }
+       
+        public void kKonturen(List<Linie> a)
+        {
+            for (int i = 0; i < a.Count; i++)
+            {
+
+            }
+        }
+        protected Point Mittelpunktrechner(Point p1, Point p2, Point p3)
+        {
+            Point mP = new Point();
+            double xZähler = ((Math.Pow(p1.X, 2) - Math.Pow(p2.X, 2)) * (p2.Y - p3.Y) - (Math.Pow(p2.X, 2) - Math.Pow(p3.X, 2)) * (p1.Y - p2.Y) + (p1.Y - p2.Y) * (p1.Y - p3.Y) * (p2.Y - p3.Y));
+            double xNenner = 2 * ((p1.X - p2.X) * (p2.Y - p3.Y) - (p2.X - p3.X) * (p1.Y - p2.Y));
+            double x = xZähler / xNenner;//könnte hier am Typcasten scheitern
+            int y = (int)((1 / 2 * (p1.Y - p2.Y)) * (Math.Pow(p1.X, 2) - Math.Pow(p2.X, 2) + Math.Pow(p1.Y, 2) - Math.Pow(p2.Y, 2) - (2 * x) * (p1.X - p2.X)));//könnte hier am Typcasten scheitern
+            mP.X = (int)x;
+            mP.Y = y;
+            return mP;
+        }
+
+
     }
 }
